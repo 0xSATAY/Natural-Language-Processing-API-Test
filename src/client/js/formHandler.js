9aec8fac9e11258299c3ab8b84319e939f1163f9
wangdiam <wadisthis26@gmail.com>
2020-05-21 23:17:29 +0800

    Updated requirements

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,11 @@
 import { checkInput } from './nameChecker';
 
-const validUrl = require('valid-url');
-
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let input = document.getElementById('name').value;
-    let formText = `https://api.aylien.com/news/stories?title="${input}"`;
-    if (checkInput(input)) {
+    let formText = document.getElementById('name').value;
+    if (checkInput(formText)) {
         console.log("::: Form Submitted :::")
         fetch('/sentiment', {
                 method: "POST",
@@ -18,7 +15,12 @@ function handleSubmit(event) {
             })
             .then(res => res.json())
             .then(function(res) {
-                document.getElementById('results').innerHTML = res.message
+                const data = res.data;
+                document.getElementById('polarity').innerHTML = data.polarity;
+                document.getElementById('polarity_confidence').innerHTML = data.polarity_confidence.toFixed(2);
+                document.getElementById('subjectivity').innerHTML = data.subjectivity;
+                document.getElementById('subjectivity_confidence').innerHTML = data.subjectivity_confidence;
+                document.getElementById('text').innerHTML = data.text;
             })
     }
 }
--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,14 +1,11 @@
+const validUrl = require('valid-url');
+
 function checkInput(inputText) {
     console.log("::: Running checkInput :::", inputText);
-    if (isEmptyOrSpaces(inputText)) {
-        alert("Please enter a valid input");
+    if (!validUrl.isUri(inputText)) {
+        alert("Please enter a valid link");
         return false;
     }
     return true;
 }
-
-function isEmptyOrSpaces(str) {
-    return str === null || str.match(/^ *$/) !== null;
-}
-
 export { checkInput }
